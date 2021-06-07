5af61df1ec6a9555f1c5c135efefefc2fafefff3
Domingo Ramirez <domingo.ramirez.artigas@gmail.com>
2021-06-06 20:11:40 -0500

    change

--- modules/get-instance-metadata-module.js
+++ modules/get-instance-metadata-module.js
@@ -4,10 +4,10 @@ module.exports.GetInstanceMetadata = async (url) => {
   return new Promise((resolve, reject) => {
     http.get('http://169.254.169.254/latest/meta-data/public-hostname', (res) => {
         const data = '';
-        resp.on('data', (chunk) => {
+        res.on('data', (chunk) => {
           data += chunk;
         });
-        resp.on('end', () => {
+        res.on('end', () => {
           resolve(data);
         });
       })
